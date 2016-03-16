59848bd12fa808ef2e9275ed66c2cfe6ff90eb9c
Andrew Kanieski <akanieski@usa.wtbts.net>
2016-03-16 16:02:05 -0400

    Fixed unit tests

--- app/controllers/thrones.js
+++ app/controllers/thrones.js
@@ -2,15 +2,30 @@
 const ThroneService = require('../services/thrones');
 
 module.exports = {
-    
+  /**
+  * @api {get} /api/thrones Read data of a User
+  * @apiVersion 0.1.0
+  * @apiName GetThrones
+  * @apiGroup Throne
+  *
+  * @apiDescription List thrones by geographic location
+  *
+  * @apiParam {Float} latitude
+  * @apiParam {Float} longitude
+  *
+  * @apiExample Example usage:
+  * curl -i http://localhost/api/thrones/47.1326132/-75.66623416
+  *
+  * @apiSuccess {Object[]} options       List of Thrones (Array of Objects).
+  */
     list: {
         secure: false,
         action: function*(req, res, next) {
             let throneService = new ThroneService(bastion.db());
-            
+
             res.ok(yield throneService.getThronesInViscinity(req.params.latitude, req.params.longitude));
-            
+
         }
     }
-    
+
 }
--- tests/users.js
+++ tests/users.js
@@ -3,24 +3,23 @@ require('./helper')
 require('mocha-generators').install()
 
 describe("User management api", function() {
-   
+
     it("must sign up a new user", function*(){
-        console.log();
         try {
             var result = yield request({
                 uri: test_url + "/api/user",
-                json: true, 
+                json: true,
                 body: {
                     Username: "testuser",
                     Password: "TestPassword00#",
                     PasswordConfirmation: "TestPassword00#",
                     Email: "test@test.com"
                 },
-                method: "POST", 
+                method: "POST",
                 resolveWithFullResponse: true,
             })
         } catch(err) {
-            var result = err;
+            var result = err.response;
             if (err.statusCode == 500) {
                 console.log(err)
                 throw err
@@ -29,8 +28,54 @@ describe("User management api", function() {
         assert.equal(result.statusCode, 200, "must return status code 200")
         assert.notEqual(result.body.token, undefined, "must return a token")
         assert.equal(result.body.success, true, "must indicate a successful sign up")
-        
-        
    })
-    
+   it("must not sign up a new user if username already is in use", function*(){
+        try {
+            var result = yield request({
+                uri: test_url + "/api/user",
+                json: true,
+                body: {
+                    Username: "testuser",
+                    Password: "TestPassword00#",
+                    PasswordConfirmation: "TestPassword00#",
+                    Email: "different@test.com"
+                },
+                method: "POST",
+                resolveWithFullResponse: true,
+            })
+        } catch(err) {
+            var result = err.response;
+            if (err.statusCode == 500) {
+                console.log(err)
+                throw err
+            }
+        }
+        assert.equal(result.statusCode, 400, "must return status code 400")
+        assert.equal(result.body.success, false, "must indicate an unsuccessful sign up")
+   })
+   it("must not sign up a new user if email already is in use", function*(){
+        try {
+            var result = yield request({
+                uri: test_url + "/api/user",
+                json: true,
+                body: {
+                    Username: "different",
+                    Password: "TestPassword00#",
+                    PasswordConfirmation: "TestPassword00#",
+                    Email: "test@test.com"
+                },
+                method: "POST",
+                resolveWithFullResponse: true,
+            })
+        } catch(err) {
+            var result = err.response;
+            if (err.statusCode == 500) {
+                console.log(err)
+                throw err
+            }
+        }
+        assert.equal(result.statusCode, 400, "must return status code 400")
+        assert.equal(result.body.success, false, "must indicate an unsuccessful sign up")
+   })
+
 })
